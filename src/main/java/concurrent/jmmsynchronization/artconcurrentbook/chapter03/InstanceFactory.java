5fa65a34f19ea8295944d1c6f21f2d4d8721c2e2
wz <296215200@qq.com>
2019-07-31 14:16:45 +0800

    art concurrency

---
+++ src/main/java/concurrent/jmmsynchronization/artconcurrentbook/chapter03/InstanceFactory.java
@@ -0,0 +1,14 @@
+package concurrent.jmmsynchronization.artconcurrentbook.chapter03;
+
+public class InstanceFactory {
+    private static class InstanceHolder {
+        public static Instance instance = new Instance();
+    }
+
+    public static Instance getInstance() {
+        return InstanceHolder.instance; //���ｫ����InstanceHolder�౻��ʼ��
+    }
+
+    static class Instance {
+    }
+}
---
+++ src/main/java/concurrent/thread/artconcurrentbook/chapter04/Synchronized.java
@@ -0,0 +1,18 @@
+package concurrent.thread.artconcurrentbook.chapter04;
+
+/**
+ * 6-10
+ */
+public class Synchronized {
+    public static void main(String[] args) {
+        // ��Synchronized Class������м���
+        synchronized (Synchronized.class) {
+
+        }
+        // ��̬ͬ����������Synchronized Class������м���
+        m();
+    }
+
+    public static synchronized void m() {
+    }
+}
