885a75c180841a82e92a66fdd62d8237a55981eb
wz <296215200@qq.com>
2019-07-31 14:05:25 +0800

    concurrency modify

---
+++ src/main/java/concurrent/jmmsynchronization/jcip/ch03/StuffIntoPublic.java
@@ -0,0 +1,18 @@
+package concurrent.jmmsynchronization.jcip.ch03;
+
+import concurrent.jmmsynchronization.jcip.ch03.Holder;
+
+/**
+ * StuffIntoPublic
+ * <p/>
+ * Unsafe publication
+ *
+ * @author Brian Goetz and Tim Peierls
+ */
+public class StuffIntoPublic {
+    public Holder holder;
+
+    public void initialize() {
+        holder = new Holder(42);
+    }
+}
